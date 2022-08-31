9e97fd5f9778e7416ee868e653c4d8d0246e7005
Ngoc-Phuc Nguyen <77053108+Saizo24@users.noreply.github.com>
2022-08-31 09:03:31 +0200

    Initialize Frontend Backend Projects

---
+++ back-spring/src/test/java/ch/tbz/m133/backspring/BackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.m133.backspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
